43a6c1f0a8c511ad405c24d7537a2f8a2659faab
Iosif Trefas <tref@systematic.com>
2019-08-02 11:49:08 +0300

    : Builder Pattern example and exercises

--- design-patterns/src/main/java/creational/builder/example/BuilderMain.java
+++ design-patterns/src/main/java/creational/builder/example/BuilderMain.java
@@ -1,13 +1,15 @@
 package creational.builder.example;
 
 
+import utils.ObjectPrintUtil;
 
 public class BuilderMain {
     public static void main(String[] args) {
-
+        Person person = new Person.Builder()
+                .setFirstName("Bob").setLastName("Marley").setMiddleName("Bobita")
+                .build();
+        ObjectPrintUtil.printObject(person);
     }
-
-
 }
 
 
--- design-patterns/src/main/java/creational/builder/example/Person.java
+++ design-patterns/src/main/java/creational/builder/example/Person.java
@@ -3,21 +3,36 @@ package creational.builder.example;
 public class Person {
     private String firstName;
     private String middleName;
+    private String lastName;
 
-    public Person(String firstName, String middleName, String lastName, int age, String fathersName, String mothersName, double height, double weight) {
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
         this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public static class Builder {
         private String firstName;
         private String middleName;
         private String lastName;
-        private int age;
-        private String fathersName;
-        private String mothersName;
-        private double height;
-        private double weight;
 
         public Builder() {
         }
@@ -39,7 +54,11 @@ public class Person {
 
 
         public Person build() {
-            return new Person(firstName, middleName, lastName, age, fathersName, mothersName, height, weight);
+            Person person = new Person();
+            person.setFirstName(firstName);
+            person.setLastName(lastName);
+            person.setMiddleName(middleName);
+            return person;
         }
     }
 }
